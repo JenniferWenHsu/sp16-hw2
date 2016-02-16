81f17a454ac7b827670fc414dfdfdc1ced09612a
Jennifer Hsu <jennifer.wen123@gmail.com>
2016-02-16 10:05:49 -0800

    complete Q2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,14 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(word)
+  	@input = word
+  end
+
+  def bar(input1, opts={})
+  	@input1 = input1
+  	@options = opts
+  	sat = @options[:sat] if @options.has_key?(:sat)
+  	dat = @options[:dat] if @options.has_key?(:dat)
+  	return "#{input1}#{@input}#{sat}"
+  end
 end
